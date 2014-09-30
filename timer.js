ba5a2cefac8d06d345374db0159ea3e0df393e41
Saren Currie <scur778@aucklanduni.ac.nz>
2014-09-30 19:35:36 +1300

    Added counting down capability

--- timer.js
+++ timer.js
@@ -5,114 +5,142 @@ var $cancelCountdown = document.querySelector(".cancel");
 
 var minutes = 15;
 var seconds = 0;
+var upwards = true;
 var countDown;
 var blinker;
 
 $timerSetupForm.onsubmit = function() {
-    minutes = Number(document.querySelector("#minutes").value);
-    seconds = Number(document.querySelector("#seconds").value);
-
-    countDown = beginCountdown(minutes, seconds);
-    return false;
+	minutes = Number(document.querySelector("#minutes").value);
+	seconds = Number(document.querySelector("#seconds").value);
+	direction = document.querySelector('input[name="direction"]:checked').value;
+	
+	if (direction == "up") {
+		upwards = true;
+	} else {
+		upwards = false;
+		$countdown.innerHTML = zeroPad(minutes, 2) + ":" + zeroPad(seconds, 2);
+	}
+	
+	countDown = beginCountdown(minutes, seconds);
+	return false;
 }
 
 $cancelCountdown.onclick = cancelCountdown;
 
 function beginCountdown(minutes, seconds) {
-    $timerSetupForm.style.left = 10000;
-    $countdownContainer.style.left = 0;
+	$timerSetupForm.style.left = 10000;
+	$countdownContainer.style.left = 0;
 
-    var totalSeconds = (60 * minutes) + seconds;
-    var secondsElapsed = 0;
-    var warned = false;
+	var totalSeconds = (60 * minutes) + seconds;
+	var secondsRemaining = totalSeconds;
+	var secondsSinceEnd = 0 - totalSeconds;
+	var initialMinutes = minutes;
+	var initialSeconds = seconds;
+	var secondsElapsed = 0;
+	var warned = false;
 
-    return setInterval(function() {
-        secondsElapsed++;
+	return setInterval(function() {
+		secondsElapsed++;
+		secondsRemaining--;
+		secondsSinceEnd++;
+		if (upwards) {
+			minutes = Math.floor(secondsElapsed / 60);
+			seconds = secondsElapsed - (minutes * 60);
+		} else {
+			if (secondsRemaining >= 0){
+				minutes = Math.floor(secondsRemaining / 60);
+				seconds = secondsRemaining - (minutes * 60);
+			} else {
+				minutes = Math.floor(secondsSinceEnd / 60);
+				seconds = secondsSinceEnd - (minutes * 60);
+			}
+		}
+		//if (upwards) {
+			$countdown.innerHTML = zeroPad(minutes, 2) + ":" + zeroPad(seconds, 2);
+		// } else {
+// 			$countdown.innerHTML = zeroPad(initialMinutes-minutes-1, 2) + ":" + zeroPad(initialSeconds-seconds+59, 2);
+// 		}
+		$countdownContainer.style.backgroundColor = "rgba(255, 0, 0, " + (secondsElapsed / totalSeconds) * (secondsElapsed / totalSeconds) + ")"
 
-        minutes = Math.floor(secondsElapsed / 60);
-        seconds = secondsElapsed - (minutes * 60);
+		if (secondsElapsed / totalSeconds >= 0.8 && !warned) {
+			beep(200, 1);
+			warned = true;
+		}
 
-        $countdown.innerHTML = zeroPad(minutes, 2) + ":" + zeroPad(seconds, 2);
-        $countdownContainer.style.backgroundColor = "rgba(255, 0, 0, " + (secondsElapsed / totalSeconds) * (secondsElapsed / totalSeconds) + ")"
+		if (secondsElapsed >= totalSeconds) {
+			beep(200, 2);
+			done = true;
+			if (!blinker) blinker = blink($countdown, 400);
 
-        if (secondsElapsed / totalSeconds >= 0.8 && !warned) {
-            beep(200, 1);
-            warned = true;
-        }
-
-        if (secondsElapsed >= totalSeconds) {
-            beep(200, 2);
-            if (!blinker) blinker = blink($countdown, 400);
-
-            // cancelCountdown();
-        }
-    }, 1000);
+			// cancelCountdown();
+		}
+	}, 1000);
 }
 
 function cancelCountdown() {
-    clearInterval(countDown);
-    clearInterval(blinker);
+	clearInterval(countDown);
+	clearInterval(blinker);
 
-    countDown = null;
-    blinker = null;
+	countDown = null;
+	blinker = null;
 
-    $countdown.style.display = "";
-    $countdownContainer.style.backgroundColor = null;
-    $timerSetupForm.style.left = 0;
-    $countdownContainer.style.left = -10000;
-    $countdown.innerHTML = "00:00";
+	$countdown.style.display = "";
+	$countdownContainer.style.backgroundColor = null;
+	$timerSetupForm.style.left = 0;
+	$countdownContainer.style.left = -10000;
+	$countdown.innerHTML = "00:00";
 }
 
 function zeroPad(number, numLength) {
-    numStr = number.toString();
+	numStr = number.toString();
 
-    while (numStr.length != numLength) {
-        numStr = "0" + numStr;
-    }
+	while (numStr.length != numLength) {
+		numStr = "0" + numStr;
+	}
 
-    return numStr;
+	return numStr;
 }
 
 function blink(element, speed) {
-    var defaultDisplay = element.style.display;
-    speed = speed || 200;
+	var defaultDisplay = element.style.display;
+	speed = speed || 200;
 
-    return setInterval(function() {
-        element.style.display = element.style.display != "none" ? "none" : defaultDisplay;
-    }, speed)
+	return setInterval(function() {
+		element.style.display = element.style.display != "none" ? "none" : defaultDisplay;
+	}, speed)
 }
 
 var beep = (function () {
-    var webaudio = window.audioContext || window.webkitAudioContext;
+	var webaudio = window.audioContext || window.webkitAudioContext;
 
-    if (webaudio) {
-        var ctx = new webaudio;
+	if (webaudio) {
+		var ctx = new webaudio;
 
-        return function (duration, type, finishedCallback) {
+		return function (duration, type, finishedCallback) {
 
-            duration = +duration;
+			duration = +duration;
 
-            // Only 0-4 are valid types.
-            type = (type % 5) || 0;
+			// Only 0-4 are valid types.
+			type = (type % 5) || 0;
 
-            if (typeof finishedCallback != "function") {
-                finishedCallback = function () {};
-            }
+			if (typeof finishedCallback != "function") {
+				finishedCallback = function () {};
+			}
 
-            var osc = ctx.createOscillator();
+			var osc = ctx.createOscillator();
 
-            osc.type = type;
+			osc.type = type;
 
-            osc.connect(ctx.destination);
-            osc.noteOn(0);
+			osc.connect(ctx.destination);
+			osc.noteOn(0);
 
-            setTimeout(function () {
-                osc.noteOff(0);
-                finishedCallback();
-            }, duration);
-        };
-    } else {
-        return function() {};
-    }
+			setTimeout(function () {
+				osc.noteOff(0);
+				finishedCallback();
+			}, duration);
+		};
+	} else {
+		return function() {};
+	}
 })();
 
